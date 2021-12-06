c094f00e71187f454f5dc5b2e2a5d3b331fed11b
Ladalimonada <63775251+Ladalimonada@users.noreply.github.com>
2021-12-06 23:20:29 +0300

    feat: tests for Sign Up Form

--- src/features/Authorization/__tests__/SignUpForm.test.jsx
+++ src/features/Authorization/__tests__/SignUpForm.test.jsx
@@ -1,32 +1,43 @@
 /* eslint-disable camelcase */
 import React from 'react';
-import {
-  render, waitFor, screen,
-} from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
 import { SignUpForm } from '../components';
 import { DICTIONARY } from '../../../shared/dictionary';
 
-describe('SignInForm ', () => {
-  it('should be rendered and submitted with correct data', async () => {
-    // Arrange
+describe('SignUpForm ', () => {
+  const handleSubmit = jest.fn((e) => e.preventDefault);
+  let inputFirstName;
+  let InputLastName;
+  let inputEmail;
+  let inputPassword;
+  let inputConfirmPassword;
+  let submitButton;
 
-    const handleSubmit = jest.fn((e) => e.preventDefault);
-
-    render(
+  beforeEach(() => {
+    const {
+      getByPlaceholderText, getByRole,
+    } = render(
       <SignUpForm onSubmit={handleSubmit} />,
     );
+    inputFirstName = getByPlaceholderText(DICTIONARY.authForm.firstName);
+    InputLastName = getByPlaceholderText(DICTIONARY.authForm.lastName);
+    inputEmail = getByPlaceholderText(DICTIONARY.authForm.email);
+    inputPassword = getByPlaceholderText(DICTIONARY.authForm.password);
+    inputConfirmPassword = getByPlaceholderText(DICTIONARY.authForm.confirmPassword);
+    submitButton = getByRole('button');
+  });
 
-    // Act
-    userEvent.type(screen.getByPlaceholderText(DICTIONARY.authForm.firstName), 'John');
-    userEvent.type(screen.getByPlaceholderText(DICTIONARY.authForm.lastName), 'Dee');
-    userEvent.type(screen.getByPlaceholderText(DICTIONARY.authForm.email), 'john.dee@someemail.com');
-    userEvent.type(screen.getByPlaceholderText(DICTIONARY.authForm.password), 'Test11234');
-    userEvent.type(screen.getByPlaceholderText(DICTIONARY.authForm.confirmPassword), 'Test11234');
-    userEvent.click(screen.getByRole('button'));
+  it('should be rendered and submitted with correct data', async () => {
+    userEvent.type((inputFirstName), 'John');
+    userEvent.type((InputLastName), 'Dee');
+    userEvent.type((inputEmail), 'john.dee@someemail.com');
+    userEvent.type((inputPassword), 'Test11234');
+    userEvent.type((inputConfirmPassword), 'Test11234');
+    userEvent.click(submitButton);
 
-    // Assert
     await waitFor(() => expect(handleSubmit).toHaveBeenCalledWith({
       confirmPassword: 'Test11234',
       email: 'john.dee@someemail.com',
@@ -34,5 +45,39 @@ describe('SignInForm ', () => {
       lastName: 'Dee',
       password: 'Test11234',
     }));
+  });
+
+  it('form should not submit if the values don`t pass the validation', async () => {
+    userEvent.type((inputEmail), 'john.deesomeemail.com');
+    userEvent.type((inputPassword), '11234');
+    userEvent.click(submitButton);
+
+    await waitFor(() => expect(handleSubmit).not.toHaveBeenCalled());
+  });
+
+  it('validation errors should appears and form should not be submitted if user click on Submit button without providing any data', async () => {
+    userEvent.click(submitButton);
+
+    expect(await screen.findByText(DICTIONARY.validationErrors.firstNameRequiered)).toBeVisible();
+    expect(await screen.findByText(DICTIONARY.validationErrors.lastNameRequiered)).toBeVisible();
+    expect(await screen.findByText(DICTIONARY.validationErrors.emailRequired)).toBeVisible();
+    expect(await screen.findByText(DICTIONARY.validationErrors.passRequired)).toBeVisible();
+    expect(await screen.findByText(DICTIONARY.validationErrors.passConfirmRequiered)).toBeVisible();
+    await waitFor(() => expect(handleSubmit).not.toHaveBeenCalled());
+  });
+
+  it('validation errors should appear if inputs were touched without typing any data', async () => {
+    userEvent.click(inputFirstName);
+    userEvent.click(InputLastName);
+    userEvent.click(inputEmail);
+    userEvent.click(inputPassword);
+    userEvent.click(inputConfirmPassword);
+    userEvent.click(submitButton);
+
+    expect(await screen.findByText(DICTIONARY.validationErrors.firstNameRequiered)).toBeVisible();
+    expect(await screen.findByText(DICTIONARY.validationErrors.lastNameRequiered)).toBeVisible();
+    expect(await screen.findByText(DICTIONARY.validationErrors.emailRequired)).toBeVisible();
+    expect(await screen.findByText(DICTIONARY.validationErrors.passRequired)).toBeVisible();
+    expect(await screen.findByText(DICTIONARY.validationErrors.passConfirmRequiered)).toBeVisible();
   });
 });
