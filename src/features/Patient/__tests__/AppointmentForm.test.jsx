3d545ae218fe4448154b561148871e8a40d87cf0
Ladalimonada <63775251+Ladalimonada@users.noreply.github.com>
2021-12-07 02:33:05 +0300

    refactoring

--- src/features/Patient/__tests__/AppointmentForm.test.jsx
+++ src/features/Patient/__tests__/AppointmentForm.test.jsx
@@ -1,22 +1,18 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable camelcase */
+// /* eslint-disable camelcase */
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import {
-  render, waitFor, screen,
+  render, screen,
 } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/dom';
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
-import MockDate from 'mockdate';
-import moment from 'moment';
 import { BrowserRouter } from 'react-router-dom';
 import { AppointmentForm } from '../components';
 import { DICTIONARY } from '../../../shared/dictionary';
-import { Button } from '../../../components';
 
 describe('SignInForm ', () => {
   let selectDoctorOccupation;
